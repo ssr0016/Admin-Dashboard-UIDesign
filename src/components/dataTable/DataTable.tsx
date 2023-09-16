f9474e7e6edebe5a9bee8afd4fdf1b905a80d37c
ssr0016 <samsonrecluta0016@gmail.com>
2023-09-16 15:49:05 +0800

    Fixing bugs

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -3,6 +3,7 @@ import {
   GridColDef,
   GridToolbar,
 } from "@mui/x-data-grid";
+import "./dataTable.scss";
 import { Link } from "react-router-dom";
 // import { useMutation, useQueryClient } from "@tanstack/react-query";
 
